f5e0017648116f9d5ac63901ab1ed0ea152a24aa
Alexander Thomas <s179069@students.aie.edu.au>
2018-01-09 09:14:54 -0800

    Add falling to player character

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,12 +6,13 @@ public class PlayerController : MonoBehaviour {
 
 
     public float moveSpeed = 10.0f;
+    public float fallAccel = 10.0f;
     public float rotationSpeed = 2.0f;
     public LayerMask playerMask;
 
     private CharacterController controller;
     private Transform camTrans;
-
+    private float fallSpeed = 0;
     
 
     private void Start()
@@ -22,7 +23,7 @@ public class PlayerController : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
+    private void Update () {
 
         Vector3 moveVec = transform.forward * Input.GetAxis("Vertical");
         moveVec += transform.right * Input.GetAxis("Horizontal");
@@ -31,6 +32,28 @@ public class PlayerController : MonoBehaviour {
         controller.Move(moveVec);
 
         
-        transform.Rotate(Quaternion.Euler(0, Input.GetAxis("Mouse X") * rotationSpeed, 0).eulerAngles);
+        transform.Rotate(Quaternion.Euler(0,
+                                          Input.GetAxis("Mouse X") * rotationSpeed,
+                                          0).eulerAngles);
+    }
+
+    private void FixedUpdate()
+    {
+        //RaycastHit hit;
+        //
+        //if(Physics.Raycast(transform.position, -transform.up, out hit, playerMask))
+        //{
+        //    transform.position = hit.point;
+        //}
+        if(!controller.isGrounded)
+        {
+            fallSpeed += fallAccel * Time.deltaTime;
+            Vector3 fallVec = -Vector3.up * fallSpeed * Time.deltaTime;
+            controller.Move(fallVec);
+        }
+        else
+        {
+            fallSpeed = 0;
+        }
     }
 }
