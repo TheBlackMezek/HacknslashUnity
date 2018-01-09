b94820efa313f066dfaf4060420ddd067208cee0
Alexander Thomas <s179069@students.aie.edu.au>
2018-01-08 16:13:26 -0800

    Initial commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float rotationSpeed = 1.0f;
+    public float baseZoom = 10.0f;
+    public float zoomSpeed = 100.0f;
+    public float verticalLookMin = 10.0f;
+    public float verticalLookMax = 60.0f;
+
+    private Transform playerTrans;
+    private float mouseYDelta = 0;
+    private float zoom;
+
+    // Use this for initialization
+    void Start () {
+        playerTrans = transform.parent.Find("PlayerBody");
+        zoom = baseZoom;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = playerTrans.position;
+
+        transform.Translate(transform.forward * -baseZoom);
+
+
+        zoom += -Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+
+        mouseYDelta += -Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseYDelta = Mathf.Clamp(mouseYDelta, verticalLookMin, verticalLookMax);
+
+        transform.position = transform.parent.position;
+        transform.rotation = Quaternion.Euler(mouseYDelta,
+                                              transform.parent.rotation.eulerAngles.y,
+                                              transform.parent.rotation.eulerAngles.z);
+        transform.position -= transform.forward * zoom;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public float moveSpeed = 10.0f;
+    public float rotationSpeed = 2.0f;
+    public LayerMask playerMask;
+
+    private CharacterController controller;
+    private Transform camTrans;
+
+
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        camTrans = transform.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        Vector3 moveVec = transform.forward * Input.GetAxis("Vertical");
+        moveVec += transform.right * Input.GetAxis("Horizontal");
+        moveVec *= moveSpeed * Time.deltaTime;
+
+        controller.Move(moveVec);
+
+        
+        transform.Rotate(Quaternion.Euler(0, Input.GetAxis("Mouse X") * rotationSpeed, 0).eulerAngles);
+    }
+}
