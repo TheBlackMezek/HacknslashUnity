dc0bae5400fbf6eb5fbf3517f56bfdd583c7f955
Alexander Thomas <s179069@students.aie.edu.au>
2018-01-09 12:37:05 -0800

    Add jump ability, add walls & platforms to level

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,30 +7,26 @@ public class PlayerController : MonoBehaviour {
 
     public float moveSpeed = 10.0f;
     public float fallAccel = 10.0f;
+    public float jumpImpulse = 30.0f;
     public float rotationSpeed = 2.0f;
     public LayerMask playerMask;
 
     private CharacterController controller;
     private Transform camTrans;
-    private float fallSpeed = 0;
-    
+    //private float fallSpeed = 0;
+    private Vector3 velocity = Vector3.zero;
+
+    public bool test;
 
     private void Start()
     {
         controller = GetComponent<CharacterController>();
+        
         Cursor.lockState = CursorLockMode.Locked;
         camTrans = transform.Find("Main Camera");
     }
-
-    // Update is called once per frame
+    
     private void Update () {
-
-        Vector3 moveVec = transform.forward * Input.GetAxis("Vertical");
-        moveVec += transform.right * Input.GetAxis("Horizontal");
-        moveVec *= moveSpeed * Time.deltaTime;
-
-        controller.Move(moveVec);
-
         
         transform.Rotate(Quaternion.Euler(0,
                                           Input.GetAxis("Mouse X") * rotationSpeed,
@@ -39,15 +35,37 @@ public class PlayerController : MonoBehaviour {
 
     private void FixedUpdate()
     {
-        if(!controller.isGrounded)
+        velocity.x = 0;
+        velocity.z = 0;
+        velocity += transform.forward * moveSpeed * Input.GetAxis("Vertical");
+        velocity += transform.right   * moveSpeed * Input.GetAxis("Horizontal");
+
+        
+        if (!controller.isGrounded)
         {
-            fallSpeed += fallAccel * Time.fixedDeltaTime;
-            Vector3 fallVec = -Vector3.up * fallSpeed * Time.fixedDeltaTime;
-            controller.Move(fallVec);
+            velocity.y -= fallAccel * Time.fixedDeltaTime;
         }
         else
         {
-            fallSpeed = 0;
+            velocity.y = 0;
+            velocity.y -= fallAccel * Time.fixedDeltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && controller.isGrounded)
+        {
+            velocity.y += jumpImpulse;
+        }
+
+
+        controller.Move(velocity * Time.fixedDeltaTime);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.normal.y < 0 && velocity.y > 0)
+        {
+            velocity.y = 0;
         }
     }
+
 }
